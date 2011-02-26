f40c5c6f9274e978e744c389cef49d36cc94dd0a
Matt Howell <misterinterrupt@gmail.com>
2011-02-26 13:45:00 -0800

    registered Project in admin.py, inlines for accomplishments to JobAdmi, Skill inlines to SkillsetAdmin

--- django_resume/resume/admin.py
+++ django_resume/resume/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-
 from models import Overview, PersonalInfo, Education, Job,\
-    Accomplishment, Skillset, Skill 
+    Accomplishment, Project, Skillset, Skill 
+
+class AccomplishmentInline(admin.StackedInline):
+    model = Accomplishment
+
+class SkillInline(admin.StackedInline):
+    model = Skill
+
+class JobAdmin(admin.ModelAdmin):
+    inlines = [
+        AccomplishmentInline,
+    ]
 
 class AccomplishmentAdmin(admin.ModelAdmin):
     list_select_related = True
     ordering = ['job__company','order']
+
+class SkillsetAdmin(admin.ModelAdmin):
+    inlines = [
+        SkillInline,
+    ]
 
 class SkillAdmin(admin.ModelAdmin):
     list_select_related = True
@@ -14,7 +29,8 @@ class SkillAdmin(admin.ModelAdmin):
 admin.site.register(PersonalInfo)
 admin.site.register(Overview)
 admin.site.register(Education)
-admin.site.register(Job)
+admin.site.register(Job, JobAdmin)
 admin.site.register(Accomplishment, AccomplishmentAdmin)
-admin.site.register(Skillset)
+admin.site.register(Project)
+admin.site.register(Skillset, SkillsetAdmin)
 admin.site.register(Skill, SkillAdmin)

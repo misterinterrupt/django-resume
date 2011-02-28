a3a014340e636b9422a8c8ecb8681b007d0eab7e
Matt Howell <misterinterrupt@gmail.com>
2011-02-28 02:28:48 -0800

    added ProjectAdmin w/ ProjectPic inline

--- django_resume/resume/admin.py
+++ django_resume/resume/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from models import Overview, PersonalInfo, Education, Job,\
-    Accomplishment, Project, Skillset, Skill 
+    Accomplishment, Project, ProjectPic, Skillset, Skill 
 
 class AccomplishmentInline(admin.StackedInline):
     model = Accomplishment
@@ -8,9 +8,18 @@ class AccomplishmentInline(admin.StackedInline):
 class SkillInline(admin.StackedInline):
     model = Skill
 
+class ProjectPicInline(admin.StackedInline):
+    model = ProjectPic
+
 class JobAdmin(admin.ModelAdmin):
     inlines = [
         AccomplishmentInline,
+    ]
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [
+        ProjectPicInline,
     ]
 
 class AccomplishmentAdmin(admin.ModelAdmin):
@@ -31,6 +40,6 @@ admin.site.register(Overview)
 admin.site.register(Education)
 admin.site.register(Job, JobAdmin)
 admin.site.register(Accomplishment, AccomplishmentAdmin)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Skillset, SkillsetAdmin)
 admin.site.register(Skill, SkillAdmin)
